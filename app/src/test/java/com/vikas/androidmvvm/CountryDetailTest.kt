3d1251e962771be3e78f7c4e0c80327f8dab64b6
Vikas Mane <vikas@relfor.com>
2019-02-18 18:48:14 +0530

    Covers CountryDetailViewModel unit tests

--- app/src/test/java/com/vikas/androidmvvm/CountryDetailTest.kt
+++ app/src/test/java/com/vikas/androidmvvm/CountryDetailTest.kt
@@ -1,11 +1,11 @@
 package com.vikas.androidmvvm
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
-import android.arch.lifecycle.*
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
 import com.vikas.androidmvvm.models.dataclasses.FactsResponse
 import com.vikas.androidmvvm.models.dataclasses.Row
 import com.vikas.androidmvvm.models.repositories.CountryDetailRepository
-import com.vikas.androidmvvm.models.services.RemoteServiceInterface
 import com.vikas.androidmvvm.viewmodels.CountryDetailViewModel
 import org.junit.Before
 import org.junit.Rule
@@ -13,41 +13,36 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
+import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
 import retrofit2.Call
 import retrofit2.Callback
-import retrofit2.Response
 
 @RunWith(MockitoJUnitRunner::class)
 class CountryDetailTest {
 
     lateinit var countryDetailViewModel: CountryDetailViewModel
     @Mock
-    lateinit var errorObserver : Observer<String>
+    lateinit var errorObserver: Observer<String>
 
     var errorLiveData = MutableLiveData<String>()
 
     @Mock
-    var countryDetailsList :MutableLiveData<List<Row?>>?=null
+    var countryDetailsList: MutableLiveData<List<Row?>>? = null
 
 
     @Mock
-    var countryDetailRepository: CountryDetailRepository?=null
+    var countryDetailRepository: CountryDetailRepository? = null
 
     @Mock
-    lateinit var serviceInterface : RemoteServiceInterface
+    lateinit var callback: Callback<FactsResponse>
 
     @Mock
-    lateinit var callback : Callback<FactsResponse>
+    lateinit var call: Call<FactsResponse>
 
     @Mock
-    lateinit var call : Call<FactsResponse>
-
-    @Mock
-    var observer : Observer<List<Row?>>? = null
+    var observer: Observer<List<Row?>>? = null
 
     @Rule
     @JvmField
@@ -60,37 +55,30 @@ class CountryDetailTest {
     }
 
     @Test
-    fun testGetCountryDetail(){
+    fun testGetCountryDetail() {
         countryDetailViewModel.getCountryDetails()
-        Mockito.verify(countryDetailRepository,Mockito.times(1))?.fetchCountryDetail(callback)
+        countryDetailRepository?.fetchCountryDetail(callback)
+        Mockito.verify(countryDetailRepository, Mockito.times(1))?.fetchCountryDetail(callback)?.enqueue(callback)
     }
+
     @Test
     fun testHandleGetCountryDetail_Success() {
-        val response = Mockito.mock(Response::class.java)
-        val apiCountryDetails = Mockito.mock(FactsResponse::class.java)
-        Mockito.doReturn(true).`when`(response).isSuccessful
-        Mockito.doReturn(apiCountryDetails).`when`(response).body()
-        var list = ArrayList<Row>()
-        list.add(Row("","",""))
-        list.add(Row("","",""))
-        list.add(Row("","",""))
-        Mockito.doReturn(list).`when`(apiCountryDetails).rows
-
+        countryDetailViewModel.getCountryDetails()
         countryDetailsList?.observeForever(observer!!)
-        countryDetailsList?.value = apiCountryDetails.rows
-        Mockito.verify(observer)?.onChanged(apiCountryDetails.rows)
+        Mockito.verify(countryDetailsList,Mockito.times(1))?.observeForever(observer!!)
     }
-    @Test
-    fun testHandleGetCountryDetail_Failure(){
-        val response = Mockito.mock(Response::class.java)
-        Mockito.doReturn(false).`when`(response).isSuccessful
 
-      //  errorLiveData.
+    @Test(expected = Throwable::class)
+    fun testHandleGetCountryDetail_Failure() {
+        val throwable = mock(Throwable::class.java)
+        countryDetailViewModel.getCountryDetails()
+        `when`(countryDetailRepository?.fetchCountryDetail(callback)).thenThrow(throwable)
+        Mockito.verify(errorLiveData)?.observeForever(errorObserver)
     }
+
     @Test
-    fun testHandleError(){
+    fun testHandleError() {
         errorLiveData.observeForever(errorObserver)
-
         errorLiveData.value = "Error"
         verify(errorObserver).onChanged("Error")
     }
